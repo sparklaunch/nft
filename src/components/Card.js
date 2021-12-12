860f976c2cb3cf39c8c8ed95e003dba248726009
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-12 10:32:48 +0900

    Add the content

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,16 @@ function Card() {
       <div className={classes.CardImage}>
         <img src="/assets/image-equilibrium.jpg" alt="Equilibrium" />
       </div>
+      <div className={classes.CardTitle}>
+        <h1>Equilibrium #3429</h1>
+      </div>
+      <div className={classes.CardContent}>
+        <p>Our Equilibrium collection promotes balance and calm.</p>
+      </div>
+      <div className={classes.CardPrice}>
+        <div className={classes.Etherium}></div>
+        <div className={classes.TimeLeft}></div>
+      </div>
     </div>
   );
 }
